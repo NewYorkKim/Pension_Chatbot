cf92a812e2222c198db8418e2a25436df146f559
NewYorkKim <newyorkkim1030@gmail.com>
2022-05-19 17:54:12 +0900

    update chatbox

--- chatbox/chatbox.js
+++ chatbox/chatbox.js
@@ -1,106 +1,46 @@
-//links
-//http://eloquentjavascript.net/09_regexp.html
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+function appendChat(user, bot){
+    console.log('Trying to append')
+    console.log(user, bot)
+    $('#botbox').append(`<div class="dialogbox user">${user}</div>
+                         <div class="dialogbox bot">${bot}</div>`);
 
-
-var messages = [], //array that hold the record of each string in chat
-  lastUserMessage = "", //keeps track of the most recent input string from the user
-  botMessage = "", //var keeps track of what the chatbot is going to say
-  botName = 'Chatbot', //name of the chatbot
-  talking = true; //when false the speach function doesn't work
-//
-//
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//edit this function to change what the chatbot says
-function chatbotResponse() {
-  talking = true;
-  botMessage = "I'm confused"; //the default message
-
-  if (lastUserMessage === 'hi' || lastUserMessage =='hello') {
-    const hi = ['hi','howdy','hello']
-    botMessage = hi[Math.floor(Math.random()*(hi.length))];;
-  }
-
-  if (lastUserMessage === 'name') {
-    botMessage = 'My name is ' + botName;
-  }
+    const $element = document.getElementById('botbox');
+    $element.scrollTop = $element.scrollHeight;
 }
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//
-//
-//
-//this runs each time enter is pressed.
-//It controls the overall input and output
-function newEntry() {
-  //if the message from the user isn't empty then run
-  if (document.getElementById("chatbox").value != "") {
-    //pulls the value from the chatbox ands sets it to lastUserMessage
-    lastUserMessage = document.getElementById("chatbox").value;
-    //sets the chat box to be clear
-    document.getElementById("chatbox").value = "";
-    //adds the value of the chatbox to the array messages
-    messages.push(lastUserMessage);
-    //Speech(lastUserMessage);  //says what the user typed outloud
-    //sets the variable botMessage in response to lastUserMessage
-    chatbotResponse();
-    //add the chatbot's name and message to the array messages
-    messages.push("<b>" + botName + ":</b> " + botMessage);
-    // says the message using the text to speech function written below
-    Speech(botMessage);
-    //outputs the last few array elements of messages to html
-    for (var i = 1; i < 8; i++) {
-      if (messages[messages.length - i])
-        document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+
+document.getElementById('chatForm').addEventListener('submit', function(e){
+        e.preventDefault();
+        userInputTag = document.getElementById('user_query');
+        user_input = userInputTag.value;
+        if(user_input == '안녕'){
+          bot_output = '만나서 반가워요.'
+        }else if(user_input == '잘가'){
+          bot_output = '함께 이야기할 수 있어서 즐거웠어요. 언제든 또 오세요 :-)'
+        }else{
+          bot_output = '죄송합니다. 잘 모르겠어요 :('
+        }
+
+        // fetch('../chat', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({name: user_input}),
+        // }).then(response => response.json()).then(some => appendChat(user_input, some))
+        // userInputTag.value = "";
+
+        appendChat(user_input, bot_output)
+        userInputTag.value = "";
+    });
+
+    function menu_selection(s){
+      user_input = s;
+      if(user_input == '관련 뉴스 조회'){
+        bot_output = '검색 키워드를 입력해 주세요.'
+      }else if(user_input == '추가메뉴2'){
+        bot_output = '서비스 예정입니다.'
+      }else{
+        bot_output = '서비스를 이용해주셔서 감사합니다. 다음에 또 오세요 :-)'
+      }
+      appendChat(user_input, bot_output)
     }
-  }
-}
-
-//text to Speech
-//https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
-function Speech(say) {
-  if ('speechSynthesis' in window && talking) {
-    var utterance = new SpeechSynthesisUtterance(say);
-    //msg.voice = voices[10]; // Note: some voices don't support altering params
-    //msg.voiceURI = 'native';
-    //utterance.volume = 1; // 0 to 1
-    //utterance.rate = 0.1; // 0.1 to 10
-    //utterance.pitch = 1; //0 to 2
-    //utterance.text = 'Hello World';
-    //utterance.lang = 'en-US';
-    speechSynthesis.speak(utterance);
-  }
-}
-
-//runs the keypress() function when a key is pressed
-document.onkeypress = keyPress;
-//if the key pressed is 'enter' runs the function newEntry()
-function keyPress(e) {
-  var x = e || window.event;
-  var key = (x.keyCode || x.which);
-  if (key == 13 || key == 3) {
-    //runs this function when enter is pressed
-    newEntry();
-  }
-  if (key == 38) {
-    console.log('hi')
-      //document.getElementById("chatbox").value = lastUserMessage;
-  }
-}
-
-//clears the placeholder text ion the chatbox
-//this function is set to run when the users brings focus to the chatbox, by clicking on it
-function placeHolder() {
-  document.getElementById("chatbox").placeholder = "";
-}
